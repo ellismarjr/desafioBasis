fbb8bb0db19f5a8b0d294a73ef76a467e77542ef
Ellismar Júnior <ellismarjr@hotmail.com>
2019-11-28 21:10:59 -0300

    fix: Request get

--- backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/Controllers/EmpresaController.java
+++ backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/Controllers/EmpresaController.java
@@ -4,12 +4,14 @@ import com.desafiobasis.com.desafiobasis.Models.Empresa;
 import com.desafiobasis.com.desafiobasis.Repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.support.CustomSQLExceptionTranslatorRegistrar;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -19,14 +21,16 @@ public class EmpresaController {
     EmpresaRepository empresaRepository;
 
 
-    @GetMapping("/empresas")
+//    @GetMapping("/empresas")
+    @RequestMapping(method = RequestMethod.GET, value = "/empresas", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public List<Empresa> find() {
         return empresaRepository.findAll();
     }
 
     @GetMapping("/empresas/{id}")
     @ResponseBody
-    public Empresa show(@PathVariable(value = "id" ) int id) {
+    public Empresa show(@PathVariable(value = "id" ) long id) {
         Empresa empresa = empresaRepository.findById(id);
 
         return empresa;
@@ -37,8 +41,16 @@ public class EmpresaController {
         return empresaRepository.save(empresa);
     }
 
-    @PutMapping("/empresas")
-    public Empresa update(@RequestBody Empresa empresa) {
+    @PutMapping(value = "/empresas/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Empresa update(@PathVariable(value = "id") long id, @RequestBody Map<String, String> body) {
+        long empresaId = id;
+        System.out.println(body.get("id"));
+        Empresa empresa = empresaRepository.findById(empresaId);
+
+        empresa.setNome(body.get(("nome")));
+        empresa.setEndereco(body.get("endereco"));
+        empresa.setCnpj(body.get("cnpj"));
+
         empresaRepository.save(empresa);
         return empresa;
     }
