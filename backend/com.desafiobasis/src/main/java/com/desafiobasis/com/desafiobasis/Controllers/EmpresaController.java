e94a55a303fad165afad3423f1cb4fd8a6343619
Ellismar Júnior <ellismarjr@hotmail.com>
2019-11-29 13:10:19 -0300

    Code refactoring | Treating http status exception error

--- backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/Controllers/EmpresaController.java
+++ backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/Controllers/EmpresaController.java
@@ -4,16 +4,13 @@ import com.desafiobasis.com.desafiobasis.Models.Empresa;
 import com.desafiobasis.com.desafiobasis.Models.dto.EmpresaDTO;
 import com.desafiobasis.com.desafiobasis.Models.mapper.EmpresaMapper;
 import com.desafiobasis.com.desafiobasis.Repository.EmpresaRepository;
+import com.desafiobasis.com.desafiobasis.error.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.support.CustomSQLExceptionTranslatorRegistrar;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200/")
@@ -34,10 +31,10 @@ public class EmpresaController {
 
     @GetMapping("/empresas/{id}")
     @ResponseBody
-    public Empresa show(@PathVariable(value = "id" ) long id) {
+    public ResponseEntity<?> show(@PathVariable(value = "id" ) long id) {
+        verifyEmpresaExists(id);
         Empresa empresa = empresaRepository.findById(id);
-
-        return empresa;
+        return new ResponseEntity<>(empresa, HttpStatus.OK);
     }
 
     @PostMapping("/empresas")
@@ -53,8 +50,16 @@ public class EmpresaController {
     }
 
     @DeleteMapping("/empresas/{id}")
-    public void destroy(@PathVariable(value = "id") long id) {
+    public ResponseEntity<?> destroy(@PathVariable(value = "id") long id) {
+        verifyEmpresaExists(id);
         empresaRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private void verifyEmpresaExists(long id) {
+        if ( empresaRepository.findById(id) == null) {
+            throw new ResourceNotFoundException("Empresa não encontrada de ID: " + id);
+        }
     }
 
 }
