a9642e6f6642479bc197f3a9bc985e172da46767
Ellismar Júnior <ellismarjr@hotmail.com>
2019-12-05 10:50:58 -0300

    Simplification annotation

--- backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/ApplicationStart.java
+++ backend/com.desafiobasis/src/main/java/com/desafiobasis/com/desafiobasis/ApplicationStart.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class ApplicationStart {
 
 	public static void main(String[] args) {
